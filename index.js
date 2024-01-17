b9cc9c3dd01b522de71e154274d70bdb06a4ba55
Dazzoo <yuraks46@gmail.com>
2024-01-17 22:31:43 +0200

    express https server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+var fs = require('fs');
+var https = require('https');
 const express = require('express')
 const app = express()
 port = 80
 
+const options = {
+    key: fs.readFileSync('../../../etc/letsencrypt/live/yurii.shushanskyi.com/privkey.pem'),
+    cert: fs.readFileSync('../../../etc/letsencrypt/live/yurii.shushanskyi.com/fullchain.pem')
+};
 
 app.get("/", async (req, res) => {
     res.end(JSON.stringify({Test: "Test response."}))
 })
 
-app.listen( port, () => {
-    console.log(`server is running on port ${port}`)
+https.createServer(options, app).listen(80, function () {
+    console.log('HTTPS server listening on port 80');
     console.log(`press CTRL+C to stop server`)
-} )
+});
